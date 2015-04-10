fec2d41f8dabf651235a311eaf2a83d4ff790436
grantziolkowski <grant.ziolkowski@gmail.com>
2015-04-10 18:00:09 -0400

    Add before save to days_til_gig

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -5,6 +5,7 @@ class Gig < ActiveRecord::Base
   has_many :practice_schedules # in case of more than one user in a band
 
   validates :date, presence: true, length: { minimum: 8, maximum: 10}
+  validates :days_til_gig, numericality: true
   validates :time, length: { minimum: 4, maximum: 7 }
   validates :difficulty, presence: true, numericality: { only_integer: true }
   validates :difficulty, numericality: { greater_than_or_equal_to: 0 }
@@ -25,12 +26,12 @@ class Gig < ActiveRecord::Base
      parse_date_string.yday - today_date.yday
   end
 
-  def days_til_gig
+  def set_days_til_gig
     if today_date.year == parse_date_string.year
-      subtract_days
+      self.days_til_gig = subtract_days
     else
       account_for_year = parse_date_string.year - today_date.year
-      account_for_year * 365 + subtract_days
+      self.days_til_gig = account_for_year * 365 + subtract_days
     end
   end
 
