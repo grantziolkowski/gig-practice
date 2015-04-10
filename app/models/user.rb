5f90a7d68a8f7ce8f67ab2f62afc4992e06a674f
Max Rater <msrater@gmail.com>
2015-04-10 14:26:08 -0400

    Took validation off of password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, presence: true
-  validates :name, presence: true
-  validates :password_digest, presence: true
   has_many :gigs
   has_many :practice_schedules
+  validates :email, presence: true
+  validates :name, presence: true
 end
