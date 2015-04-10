aa975cfd516e10292439e7b961127ebce67acd21
Max Rater <msrater@gmail.com>
2015-04-10 14:20:01 -0400

    Added Vigrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :password_digest, presence: true
   has_many :gigs
   has_many :practice_schedules
 end
