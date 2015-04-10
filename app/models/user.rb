4c42c3601bf2a1b6478ccd4cffade1e2bc371dd4
grantziolkowski <grant.ziolkowski@gmail.com>
2015-04-10 19:40:04 -0400

    Take out validations for testing, fix signout routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :gigs
   has_many :practice_schedules
-  validates :email, presence: true, uniqueness: true
-  validates :name, presence: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :name, presence: true
 
   def authentic?(input_password)
     user && user.authenticate(input_password)
