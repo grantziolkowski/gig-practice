f8f0befe289e2b5ad9d93f835ce82e4e915da301
grantziolkowski & Lucian Kahn & Max Rater <pair+grantziolkowski.luciankahn.maxrater@devbootcamp.com>
2015-04-10 15:14:17 -0400

    helper for authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :gigs
   has_many :practice_schedules
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :name, presence: true
+
+  def authentic?(input_password)
+    user && user.authenticate(input_password)
+  end
+
 end
