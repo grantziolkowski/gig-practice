b2c8bc41bc4ab7e7615dd3215359798c42cb4e2a
grantziolkowski & Lucian Kahn & Max Rater <pair+grantziolkowski.luciankahn.maxrater@devbootcamp.com>
2015-04-10 15:02:37 -0400

    views for sign up and sign in, extract partial for repeated fields

---
+++ app/controllers/auth.rb
@@ -0,0 +1,31 @@
+auth.rb
+
+# see sign up form
+get '/signup' do
+
+erb :'auth/signup'
+end
+
+# submit sign up form
+
+post '/signup' do
+
+end
+
+# see sign in form
+
+get '/signin' do
+
+end
+
+# submit sign in form
+
+post '/signin' do
+
+end
+
+# submit sign out
+
+post '/signout' do
+
+end
