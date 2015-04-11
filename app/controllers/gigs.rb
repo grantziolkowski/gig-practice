e04503cdc77d2412a9fa1d506dbe35ed134901af
Lucian Kahn <luciankahn@gmail.com>
2015-04-11 12:50:08 -0400

    fix gig routes

--- app/controllers/gigs.rb
+++ app/controllers/gigs.rb
@@ -1,32 +1,32 @@
 # see form to: add a new gig for the current user
-get 'gigs/new' do
+get '/gigs/new' do
   @user_id = current_user
-  erb :'/gigs/new.erb'
+  erb :'gigs/new'
 end
 
 # add a new gig for the current user
-post 'gigs/:user_id' do
+post '/gigs/:user_id' do
   Gig.new(name: params['name'], date: params['date'], difficulty: difficulty_helper, user_id: params['user_id'])
   redirect '/gigs/:user_id'
 end
 
 # see form to: edit a gig for the current user / error & redirect if it's the wrong user
-get 'gigs/edit/:gig_id' do
+get '/gigs/edit/:gig_id' do
 
 end
 
 # post: edit a gig for the current user / error & redirect if it's the wrong user
-put 'gigs/edit/:gig_id' do
+put '/gigs/edit/:gig_id' do
 
 end
 
 # see form to: delete a gig for the current user / error & redirect if it's the wrong user
-get 'gigs/delete/:gig_id' do
+get '/gigs/delete/:gig_id' do
 
 end
 
 # delete a gig for the current user / error & redirect if it's the wrong user
-delete 'gigs/delete/:gig_id' do
+delete '/gigs/delete/:gig_id' do
 
 end
 
@@ -38,7 +38,7 @@ get '/gigs/:user_id' do |user_id|
 end
 
 # see one gig for a user
-get 'gigs/:user_id/:gig_id' do |user_id, gig_id|
+get '/gigs/:user_id/:gig_id' do |user_id, gig_id|
 
 end
 
--- app/helpers/difficulty_helper.rb
+++ app/helpers/difficulty_helper.rb
@@ -1,7 +1,3 @@
 def difficulty_helper
-  case params['difficulty']
-    when 'easy' then return 0
-    when 'medium' then return 1
-    when 'hard' then return 2
-  end
+  params['difficulty'].to_i
 end
